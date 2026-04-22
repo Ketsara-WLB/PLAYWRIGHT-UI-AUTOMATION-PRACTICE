441720a60188b5fadc1f88140633fa9927d1331c
ketsara <ketsara.piy@ku.th>
2026-04-22 15:16:24 +0700

    [Added]: เพิ่มLogin Page: Invalid Password ใน Login.spec.ts

--- tests/Login.spec.ts
+++ tests/Login.spec.ts
@@ -37,3 +37,28 @@ test('Login Success', async ({ page }) => {
     await expect(page.getByTestId('success-heading')).toHaveText('Login successful');
   });
 })
+
+test('Login Page: Invalid Password', async ({ page }) => {
+  await test.step('navigate ไปหน้า login', async () => {
+      await page.goto('https://ui-sandbox-omega.vercel.app');
+      await page.getByRole('link', { name: 'Login Form Practice login' }).click()
+    });
+  
+    await test.step('กรอก email', async () => {
+      const emailInput = page.getByTestId('email-input')
+      await expect(emailInput).toBeVisible()
+      await emailInput.fill('user@company.com')
+    });
+  
+    await test.step('กรอก password', async () => {
+      await page.getByTestId('password-input').fill('Test1234')
+    });
+  
+    await test.step('กดปุ่ม login', async () => {
+      await page.getByTestId('login-btn').click()
+    });
+
+  // Validate
+  await expect(page.getByTestId('password-error')).toHaveText('Incorrect email or password.') 
+  
+})
