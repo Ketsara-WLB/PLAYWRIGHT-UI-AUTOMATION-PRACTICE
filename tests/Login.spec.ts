d10182486dc0a235cb6f7631ce64b83d15545740
ketsara <ketsara.piy@ku.th>
2026-04-24 16:40:46 +0700

    [Added]: เพิ่ม Test Fucnction Login Page: Invalid Email, Login Page: Invalid Email, Invalid Password, Login Page: Empty

--- tests/Login.spec.ts
+++ tests/Login.spec.ts
@@ -52,7 +52,7 @@ test('Login Page: Invalid Password', async ({ page }) => {
   });
   
   await test.step('กรอก password', async () => {
-    await page.getByTestId('password-input').fill('Test1234')
+    await page.getByTestId('password-input').fill('Invalid1234!')
   });
   
   await test.step('กดปุ่ม login', async () => {
@@ -65,5 +65,93 @@ test('Login Page: Invalid Password', async ({ page }) => {
 
     await expect(page.getByTestId('toast-title')).toHaveText('Login failed')
     await expect(page.getByTestId('toast-msg')).toHaveText('Invalid email or password. Please try again.')
+  });
+})
+
+// 4 นาที
+test('Login Page: Invalid Email', async ({ page }) => {
+  await test.step('navigate ไปหน้า login', async () => {
+      await page.goto('https://ui-sandbox-omega.vercel.app');
+      await page.getByRole('link', { name: 'Login Form Practice login' }).click()
+    });
+  
+  await test.step('กรอก email', async () => {
+    const emailInput = page.getByTestId('email-input')
+    await expect(emailInput).toBeVisible()
+    await emailInput.fill('invalid@company.com')
+  });
+  
+  await test.step('กรอก password', async () => {
+    await page.getByTestId('password-input').fill('Test1234!')
+  });
+  
+  await test.step('กดปุ่ม login', async () => {
+    await page.getByTestId('login-btn').click()
+  });
+
+  // Validate
+  await test.step('Validate แจ้งเตือนการกรอก Email ผิด', async () => {
+    await expect(page.getByTestId('password-error')).toHaveText('Incorrect email or password.') 
+
+    await expect(page.getByTestId('toast-title')).toHaveText('Login failed')
+    await expect(page.getByTestId('toast-msg')).toHaveText('Invalid email or password. Please try again.')
+  });
+})
+
+// 2 นาที
+test('Login Page: Invalid Email, Invalid Password', async ({ page }) => {
+  await test.step('navigate ไปหน้า login', async () => {
+      await page.goto('https://ui-sandbox-omega.vercel.app');
+      await page.getByRole('link', { name: 'Login Form Practice login' }).click()
+    });
+  
+  await test.step('กรอก email', async () => {
+    const emailInput = page.getByTestId('email-input')
+    await expect(emailInput).toBeVisible()
+    await emailInput.fill('invalid@company.com')
+  });
+  
+  await test.step('กรอก password', async () => {
+    await page.getByTestId('password-input').fill('Invalid1234!')
+  });
+  
+  await test.step('กดปุ่ม login', async () => {
+    await page.getByTestId('login-btn').click()
+  });
+
+  // Validate
+  await test.step('Validate แจ้งเตือนการกรอก Email และ Password ผิด', async () => {
+    await expect(page.getByTestId('password-error')).toHaveText('Incorrect email or password.') 
+
+    await expect(page.getByTestId('toast-title')).toHaveText('Login failed')
+    await expect(page.getByTestId('toast-msg')).toHaveText('Invalid email or password. Please try again.')
+  });
+})
+
+//5 นาที
+test('Login Page: Empty', async ({ page }) => {
+  await test.step('navigate ไปหน้า login', async () => {
+      await page.goto('https://ui-sandbox-omega.vercel.app');
+      await page.getByRole('link', { name: 'Login Form Practice login' }).click()
+    });
+  
+  await test.step('กรอก email', async () => {
+    const emailInput = page.getByTestId('email-input')
+    await expect(emailInput).toBeVisible()
+    await emailInput.fill('')
+  });
+  
+  await test.step('กรอก password', async () => {
+    await page.getByTestId('password-input').fill('')
+  });
+  
+  await test.step('กดปุ่ม login', async () => {
+    await page.getByTestId('login-btn').click()
+  });
+
+  // Validate
+  await test.step('Validate แจ้งเตือนการกรอกค่าว่าง', async () => {
+    await expect(page.getByTestId('email-error')).toHaveText('Please enter a valid email address.') 
+    await expect(page.getByTestId('password-error')).toHaveText('Password is required.') 
   });
 })
