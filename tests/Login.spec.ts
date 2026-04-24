1b107f5177bb851c798a7c04388187f3efb8d505
ketsara <ketsara.piy@ku.th>
2026-04-24 10:04:28 +0700

    [Added]: เพิ่ม test step Validate แจ้งเตือนการกรอกรหัสผิด บน Login.spec.ts

--- tests/Login.spec.ts
+++ tests/Login.spec.ts
@@ -38,27 +38,32 @@ test('Login Success', async ({ page }) => {
   });
 })
 
+// 51นาที
 test('Login Page: Invalid Password', async ({ page }) => {
   await test.step('navigate ไปหน้า login', async () => {
       await page.goto('https://ui-sandbox-omega.vercel.app');
       await page.getByRole('link', { name: 'Login Form Practice login' }).click()
     });
   
-    await test.step('กรอก email', async () => {
-      const emailInput = page.getByTestId('email-input')
-      await expect(emailInput).toBeVisible()
-      await emailInput.fill('user@company.com')
-    });
+  await test.step('กรอก email', async () => {
+    const emailInput = page.getByTestId('email-input')
+    await expect(emailInput).toBeVisible()
+    await emailInput.fill('user@company.com')
+  });
   
-    await test.step('กรอก password', async () => {
-      await page.getByTestId('password-input').fill('Test1234')
-    });
+  await test.step('กรอก password', async () => {
+    await page.getByTestId('password-input').fill('Test1234')
+  });
   
-    await test.step('กดปุ่ม login', async () => {
-      await page.getByTestId('login-btn').click()
-    });
+  await test.step('กดปุ่ม login', async () => {
+    await page.getByTestId('login-btn').click()
+  });
 
   // Validate
-  await expect(page.getByTestId('password-error')).toHaveText('Incorrect email or password.') 
-  
+  await test.step('Validate แจ้งเตือนการกรอกรหัสผิด', async () => {
+    await expect(page.getByTestId('password-error')).toHaveText('Incorrect email or password.') 
+
+    await expect(page.getByTestId('toast-title')).toHaveText('Login failed')
+    await expect(page.getByTestId('toast-msg')).toHaveText('Invalid email or password. Please try again.')
+  });
 })
